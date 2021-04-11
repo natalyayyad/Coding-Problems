60d6258c1a60e438c8fad0a663be9ed2de017038
Nataly Ayyad <nataly.ayyad@gmail.com>
2021-04-11 23:43:30 +0300

    myLeetCodeAnswers

--- src/hard/LongestValidParentheses.java
+++ src/hard/LongestValidParentheses.java
@@ -1,5 +1,46 @@
 package hard;
 
 public class LongestValidParentheses {
+	public int longestValidParentheses(String s) {
+		int push=0, pop=0, max=0;
 
+		if(s.isEmpty() || s.length()==1)
+			return 0;
+
+		for(int i=0; i< s.length(); i++) {
+			if(s.charAt(i) == '(')
+				push++;
+			else
+				pop++;
+
+			if(pop == push){
+				int len = pop+push;
+				max = Math.max(len,max);
+			}
+			else if(pop >push) {
+				pop=0;
+				push=0;
+			}	
+		}
+		pop=0;
+		push=0;
+
+		for(int i=s.length()-1; i>0; i--) {
+			if(s.charAt(i) == '(')
+				push++;
+			else
+				pop++;
+
+			if(pop == push){
+				int len = pop+push;
+				max = Math.max(len,max);
+			}
+			else if(push >pop) {
+				pop=0;
+				push=0;
+			}
+		}
+
+		return max;
+	}
 }
